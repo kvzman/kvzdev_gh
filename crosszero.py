f58b311bc1f2eb72df205446376263cb31c090a1
kvzman <vt280789@gmail.com>
2022-11-08 23:11:05 +0300

    Code for test

--- crosszero.py
+++ crosszero.py
@@ -1,65 +1,86 @@
-# print('  1 2 3\n1 - - -\n2 - - -\n3 - - -')
 
-# input("ход первого игрока")
-# input("ход второго игрока")
+matrix = [["-"] * 3 for i in range(3)]
+move_num = 0
 
-matrix = [
-    [' ', 1, 2, 3],
-    [1, "-", "-", "-"],
-    [2, "-", "-", "-"],
-    [3, "-", "-", "-"],
-]
+def hello():
+    print("________________________________________________________")
+    print("                  Добро пожаловать!                     ")
+    print("              Эта игра крестики-нолики                  ")
+    print("________________________________________________________")
+    print("Чтобы сделать ход, введите координаты X и Y через пробел")
+    print("X - номер строки.                                       ")
+    print("Y - номер столбца.                                      ")
 def print_matrix():
-    for row in matrix:
-        for el in row:
-            print(el, end=" ")
-        print()
+    print()
+    print(f"  1 2 3")
+    for i in range(3):
+        print(f"{i+1} {matrix[i][0]} {matrix[i][1]} {matrix[i][2]}")
+    print()
 
-def cross(pl1_row, pl1_el):
-    if matrix[pl1_row][pl1_el] == "-":
-        matrix[pl1_row][pl1_el] = "X"
+def player_move():
+    while True:
+        move = input("      Ваш ход  ").split()
+        if len(move) != 2:
+            print("введите 2 координаты через пробел в диапазоне от 1 до 3")
+            continue
+        x, y = move
+
+        if not(x.isdigit()) or not(y.isdigit()):
+            print("введите 2 координаты через пробел в диапазоне от 1 до 3")
+            continue
+        x, y = int(x), int(y)
+
+        if 1>x>3 and 1>y>3:
+            print("введите 2 координаты через пробел в диапазоне от 1 до 3")
+            continue
+
+        if matrix[x-1][y-1] != '-':
+            print("Эта клетка занята!")
+            continue
+        return x-1, y-1
+
+def win_check():
+    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 0), (1, 0), (2, 0)), ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)),
+                ((0, 0), (1, 1), (2, 2)),((2, 0), (1, 1), (0, 2)))
+
+    for comb in win_cord:
+        win_combo = []
+        for i in comb:
+            win_combo.append(matrix[i[0]][i[1]])
+
+        if win_combo == ["X", "X", "X"]:
+            print_matrix()
+            print("Победил крестик!")
+            return  True
+
+        if win_combo == ["O", "O", "O"]:
+            print_matrix()
+            print("Победил нолик!")
+            return True
+
+    return False
+
+hello()
+while True:
     print_matrix()
-    return matrix
 
-def circle(pl2_row, pl2_el):
-    if matrix[pl2_row][pl2_el] == "-":
-        matrix[pl2_row][pl2_el] = "O"
-    print_matrix()
-    return matrix
-print(matrix)
+    move_num += 1
+    if move_num%2 == 1:
+        print("Ходит крестик!")
+    else:
+        print("Ходит нолик!")
 
-def win_cross():
-    if matrix[1][1] == "X" and matrix[1][2] == "X" and matrix[1][3] == "X":
-        print("первый игрок победил")
-    elif matrix[2][1] == "X" and matrix[2][2] == "X" and matrix[2][3] == "X":
-        print("первый игрок победил")
-    elif matrix[3][1] == "X" and matrix[3][2] == "X" and matrix[3][3] == "X":
-        print("первый игрок победил")
-    elif matrix[1][1] == "X" and matrix[2][1] == "X" and matrix[3][1] == "X":
-        print("первый игрок победил")
-    elif matrix[1][2] == "X" and matrix[2][2] == "X" and matrix[3][2] == "X":
-        print("первый игрок победил")
-    elif matrix[1][3] == "X" and matrix[2][3] == "X" and matrix[3][3] == "X":
-        print("первый игрок победил")
-    elif matrix[1][1] == "X" and matrix[2][2] == "X" and matrix[3][3] == "X":
-        print("первый игрок победил")
-    elif matrix[1][3] == "X" and matrix[2][2] == "X" and matrix[3][1] == "X":
-        print ("первый игрок победил")
+    x, y = player_move()
 
+    if move_num % 2 == 1:
+        matrix[x][y] = "X"
+    else:
+        matrix[x][y] = "O"
 
-for i in range(9):
-    player_1 = input("ход первого игрока")
-    pl1_row = int(player_1[0])
-    pl1_el = int(player_1[1])
-    cross(pl1_row, pl1_el)
-    win_cross()
+    if win_check():
+        break
 
-    player_2 = input("ход второго игрока")
-    pl2_row = int(player_2[0])
-    pl2_el = int(player_2[1])
-    circle(pl2_row, pl2_el)
-
-
-
-
-
+    if move_num == 9:
+        print("Ничья!")
+        break
